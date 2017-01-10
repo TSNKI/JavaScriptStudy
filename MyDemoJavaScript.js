c1b1e71657a02e63a94ba38742f4eae2fb7b05ea
TSNKI <929509707@qq.com>
2017-01-10 22:30:05 +0800

    first trying

---
+++ MyDemoJavaScript.js
@@ -0,0 +1,8 @@
+/**
+ * Created by 92950_000 on 2017/1/10 0010.
+ */
+
+function myFunction() {
+    document.getElementById("demo1").innerText = "Surprise motherfucker!"
+    document.getElementById("demo2").innerHTML = "<span style='color:blue'>test1</span> test2";
+}
